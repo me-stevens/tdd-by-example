162864f8a78c0355012fba17a9e63cf1c9d35fd6
private <user@example.com>
2015-10-14 17:15:16 +0100

    Add a test and class for Francs to illustrate that you should make small tests that represent progress when you can't figure out the big picture.

--- test/MultiCurrencyMoney.java
+++ test/MultiCurrencyMoney.java
@@ -16,6 +16,11 @@ public class MultiCurrencyMoney {
         assertEquals(new Dollar(15), five.times(3));
     }
 
+    @Test
+    public void francMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
     @Test
