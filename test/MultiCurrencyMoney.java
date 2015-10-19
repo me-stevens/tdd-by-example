af55324066021d32691c0b8028c723af71b4b858
private <user@example.com>
2015-10-19 16:37:52 +0100

    Deletes the Franc and Dollar classes and their associated tests.

--- test/MultiCurrencyMoney.java
+++ test/MultiCurrencyMoney.java
@@ -10,38 +10,22 @@ import static org.junit.Assert.assertTrue;
 public class MultiCurrencyMoney {
 
     @Test
-    public void multiplication() {
+    public void dollarMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
-    public void francMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void equality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-
-        assertTrue(Money.franc(5).equals(new Franc(5, "CHF")));
-        assertFalse(Money.franc(5).equals(new Franc(6, "CHF")));
-
-        assertFalse(Money.dollar(5).equals(new Franc(6, "CHF")));
+        assertFalse(Money.dollar(5).equals(Money.franc(6)));
     }
 
     @Test
     public void currency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
