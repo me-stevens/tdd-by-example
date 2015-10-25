e59fbb760c067365d3ee7858a97a93ed33b77d1c
private <user@example.com>
2015-10-25 13:01:59 +0000

    Add the TestCaseTest class and modify the WasRun class to make the test pass.

---
+++ xunitPython/test_case_test.py
@@ -0,0 +1,13 @@
+import unittest
+from was_run   import WasRun
+from test_case import TestCase
+
+class TestCaseTest(TestCase):
+
+  def testRunning(self):
+    test = WasRun("testMethod")
+    assert(not test.wasRun)
+    test.run()
+    assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
--- xunitPython/was_run.py
+++ xunitPython/was_run.py
@@ -4,8 +4,13 @@ class WasRun:
 
   def __init__(self, name):
     self.wasRun = None
-    TestCase.__init__(self, name)
+    self.name   = name
+    #TestCase.__init__(self, name)
+    testcase    = TestCase(name)
 
   def testMethod(self):
     self.wasRun = 1
 
+  def run(self):
+    method = getattr(self, self.name)
+    method()
